339e3666079a7dfc8589bc41c8bd27629e7bf5ff
amonooe <amonooe@sunypoly.edu>
2025-12-01 06:07:31 -0500

    commrents commit

--- components/ui/Comments.jsx
+++ components/ui/Comments.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, Image, TouchableOpacity, TextInput, ScrollView, FlatList, Animated, Pressable} from 'react-native'
+import React, { useCallback, useEffect, useState, useRef  } from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { supabase } from '../../lib/supabase';
@@ -8,8 +8,41 @@ import { supabase } from '../../lib/supabase';
 export default function Comments({post_id}) {
     const {session, profile} = useAuth()
     const [userComment, setUserComment]= useState('')
-    const [comments, setComments] = useState()
+    const [comments, setComments] = useState([])
+    const [pendingComments, setPendingComments] = useState(new Set())
+    const [newCommentsCount, setNewCommentsCount] = useState(0)
+    const [visibleComments, setVisibleComments] = useState(new Set())
+    const [newCommentIds, setNewCommentIds] = useState(new Set())
+    const [replyingTo, setReplyingTo] = useState()
+    const flatListRef = useRef(null)
+    const timeoutRefs = useRef({})
+    const fadeAnims = useRef({})
 
+    const onViewableItemsChanged = useCallback(({viewableItems}) => {
+        const currentlyVisible = new Set(viewableItems.map(item => item.item.id))
+        setVisibleComments(currentlyVisible)
+
+        setPendingComments(prev =>{
+          const stillPending = new Set()
+
+          prev.forEach(id => {
+            if(currentlyVisible.has(id)){
+              activateHighlight(id)
+              console.log('visible', id)
+              setNewCommentsCount(prev => Math.max(0, prev - 1))
+
+            }
+            else{
+              stillPending.add(id)
+            }
+          })
+          return stillPending
+        })
+    },[])
+
+    const visibilityConfig = useRef({
+        itemVisiblePercentThreshold: 10
+    }).current
     const sendComment = async (commentData) =>{
         const {data, error}= await supabase.from('social_player_comments').insert(commentData)
         if(error){
@@ -21,7 +54,7 @@ export default function Comments({post_id}) {
     }
 
     const scrollToNewComments = ()=>{
-      
+      flatListRef.current.scrollToIndex({index: 0, animated: true})
     }
 
     useEffect(()=>{
@@ -33,7 +66,9 @@ export default function Comments({post_id}) {
           console.log(error)
         }
         else{
+          console.log(data)
           setComments(data)
+
         }
       }
       if(!post_id)return
@@ -57,7 +92,10 @@ export default function Comments({post_id}) {
           .eq('id', payload.new.id).single()
 
           if(newComments){
-            setComments(prev => [...prev, newComments ])
+            setComments(prev => [ newComments, ...prev ])
+            setPendingComments(prev => new Set(prev).add(newComments.id))
+            setNewCommentsCount(prev => prev + 1)
+             
           }
 
 
@@ -68,19 +106,54 @@ export default function Comments({post_id}) {
     
     }, [post_id])
 
+  const activateHighlight = (commentId) =>{
+    setNewCommentIds(prev => new Set(prev).add(commentId))
+    fadeAnims.current[commentId] = new Animated.Value(1)
 
-  return (
-    <View className='flex-1 px-5 mb-5 relative'>
-      <Text>Comments</Text>
-      <View className='absolute z-10 top-5 left-1/2 -translate-x-1/2'>
-          <TouchableOpacity className='p-4  rounded-full'style={{backgroundColor: '#A477C7'}}>
-            <Text className='text-white font-supreme'> New Comments</Text>
-          </TouchableOpacity>
-        </View>
-      <ScrollView className=''>
-       {!comments ? <Text>Loading...</Text>:
-       comments.map((comment)=>(
-        <View className="flex flex-row p-3 gap-3">
+    timeoutRefs.current[commentId] = setTimeout(() => {
+      Animated.timing(fadeAnims.current[commentId], {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: false
+      }).start(() => {
+        clearHighlight(commentId)
+      })
+    })
+  }
+
+  const clearHighlight = (commentId) => {
+    setNewCommentIds(prev =>{
+      const newSet = new Set(prev)
+      newSet.delete(commentId)
+      return newSet
+    })
+    if (timeoutRefs.current[commentId]) {
+      clearTimeout(timeoutRefs.current[commentId])
+      delete timeoutRefs.current[commentId]
+    }
+    if(fadeAnims.current[commentId]){
+      delete fadeAnims.current[commentId]
+    }
+  }
+
+  const renderComments = ({item: comment}) =>{ 
+    if(!comment)return
+    const isNew= newCommentIds.has(comment?.id)
+    const fadeAnim = fadeAnims.current[comment?.id]
+
+    const backgroundColor = isNew && fadeAnim? fadeAnim.interpolate({
+      inputRange:[0, 1], outputRange: ['#A477C7', 'white']
+    }):
+    'transparent'
+    return (
+    <Animated.View 
+      style={{
+        backgroundColor,
+        borderWidth: isNew ? 1 : 0,
+        borderColor: isNew ? 'white' : 'transparent',
+      }}
+    >
+      <View className='flex flex-row items-center gap-2 p-3 ' >
           <Image source={{uri: comment.user?.profile_pic}} style={{width:40, height:40}}/>
           <View className="gap-2">
             <View className= 'flex flex-row justify-between'>
@@ -88,16 +161,38 @@ export default function Comments({post_id}) {
               <Text >{comment.created_at}</Text>
             </View>
             <Text className='font-supreme'>{comment.comment}</Text>
-            <Text>Reply</Text>
+            <Pressable
+              onPress={()=> setReplyingTo(comment.id)}
+            >
+              <Text>Reply</Text>
+            </Pressable>
           </View>
         </View>
-       ))
-       
-       }
-      </ScrollView>
+    </Animated.View>
+  )}
+  return (
+    <View className='flex-1 px-5 mb-5 relative'>
+      <Text>Comments</Text>
+      {newCommentsCount > 0 && <View className='absolute z-10 top-5 left-1/2 -translate-x-1/2'>
+          <TouchableOpacity className='p-4  rounded-full'style={{backgroundColor: '#A477C7'}}
+          onPress={scrollToNewComments}
+          >
+            <Text className='text-white font-supreme'>{newCommentsCount} New Comments</Text>
+          </TouchableOpacity>
+        </View>}
+     
+       <FlatList
+       ref={flatListRef}
+       data={comments}
+       renderItem={renderComments}
+       keyExtractor={(item) => item.id}
+       onViewableItemsChanged={onViewableItemsChanged}
+       ListEmptyComponent={<Text>Loading...</Text>}
+       viewabilityConfig={visibilityConfig}
+       />
 
-    
-        {session &&
+        {session &&(
+            {replyingTo && <View>}
               <View className ='flex flex-row items-center gap-3 '>
             <Image source={{ uri: profile?.profile_pic }} className='rounded-full' style={{width:35, height:35}}/>
             <View className='flex flex-row rounded-full items-center p-3 flex-1' style={{borderWidth: 1}}>
@@ -121,7 +216,7 @@ export default function Comments({post_id}) {
                 <Ionicons name="send" size={24} color="black" />                
                 </TouchableOpacity>
             </View>
-          </View> 
+          </View> )
         }
       </View>
    
